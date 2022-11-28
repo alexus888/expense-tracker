e704f8fe7bf91229497e7b6ecc8418e07e7a98f0
Alexus Wong <awong@aledade.com>
2022-11-28 10:45:35 -0600

    Add babel and topLevelAwait to webpack config
     - add serve ui|db commands as well

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,26 @@ module.exports = {
     devServer: {
         static: path.join(__dirname, 'dist')
     },
+    experiments: {
+        topLevelAwait: true
+    },
     "module": {
         "rules": [{
             "test": /\.css$/,
             "use": ["style-loader",
                 "css-loader"
             ]
-        },]
+        },
+        {
+            "test": /\.js$/,
+            "exclude": /node_modules/,
+            "use": {
+                "loader": "babel-loader",
+                "options": {
+                    "presets": ["@babel/preset-env",]
+                }
+            }
+        }
+    ]
     }
 }
