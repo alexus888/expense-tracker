1ff4c75cc86d80b97e00b3bb7522ef1373d47d00
Alexus Wong <awong@aledade.com>
2022-12-04 21:31:20 -0600

    Add cli utility for json db setup and teardown

---
+++ lib/utilities/cli.py
@@ -0,0 +1,28 @@
+import click
+import json
+from os.path import dirname, join
+
+
+@click.group
+def main() -> None:
+    ...
+
+
+@main.command
+def get_totals():
+    transactions = get_transactions()
+    balance = sum([t.get("amount") for t in transactions])
+    income = sum([t.get("amount") for t in transactions if t.get("amount") > 0])
+    expense = sum([t.get("amount") for t in transactions if t.get("amount") < 0])
+    click.echo(json.dumps({"balance": balance, "income": income, "expense": expense}))
+
+
+def get_transactions():
+    json_db_path = dirname(dirname(dirname(__file__)))  # TODO make this less janky
+    with open(join(json_db_path, "db.json"), "r") as file:
+        transactions = json.loads(file.read()).get("transactions")
+        return transactions
+
+
+if __name__ == "__main__":
+    main()
