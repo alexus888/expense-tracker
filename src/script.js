151c8f6ab280fbac77df369c7b14f318734a1c11
Alexus Wong <awong@aledade.com>
2022-12-05 22:39:27 -0600

    Refactor updateTotals function
     - rename variables and move variables into func that are only used once

--- src/script.js
+++ src/script.js
@@ -1,9 +1,7 @@
 import './style.css';
 import { v4 as uuidv4 } from 'uuid';
 
-const balance = document.getElementById('balance');
-const money_plus = document.getElementById('money-plus');
-const money_minus = document.getElementById('money-minus');
+
 const list = document.getElementById('list');
 const form = document.getElementById('form');
 const text = document.getElementById('text');
@@ -89,23 +87,28 @@ function addTransactionDOM(transaction) {
 
 // Update the balance, income and expense
 function updateTotals() {
+  const balance = document.getElementById('balance');
+  const income = document.getElementById('money-plus');
+  const expense = document.getElementById('money-minus');
   const amounts = transactions.map(transaction => transaction.amount);
 
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  const total_value = amounts
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
 
-  const income = amounts
+  const income_value = amounts
     .filter(item => item > 0)
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
 
-  const expense = (
-    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
-    -1
-  ).toFixed(2);
+  const expense_value = (
+    amounts.filter(item => item < 0)
+    .reduce((acc, item) => (acc += item), 0) * -1)
+    .toFixed(2);
 
-  balance.innerText = `$${total}`;
-  money_plus.innerText = `$${income}`;
-  money_minus.innerText = `$${expense}`;
+  balance.innerText = `$${total_value}`;
+  income.innerText = `$${income_value}`;
+  expense.innerText = `$${expense_value}`;
 }
 
 // Remove transaction by ID
