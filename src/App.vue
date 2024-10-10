05fbea02490d6ab37586c545609789b88964c1a1
Alexus Wong <alexus888@gmail.com>
2024-10-10 14:33:59 -0500

    Move template to top of component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,57 @@
-<script setup lang="ts">
+<template>
+    <div class="container">
+      <h4>Your Balance</h4>
+      <h1 id="balance" data-selector="balance">$0.00</h1>
+
+      <div class="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+          <p id="money-plus" data-selector="income" class="money plus">+$0.00</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+          <p id="money-minus" data-selector="expense" class="money minus">-$0.00</p>
+        </div>
+      </div>
+
+      <h3>History</h3>
+      <ul id="list" class="list" data-selector="history">
+      </ul>
+
+      <h3>Add new transaction</h3>
+      <form id="form">
+        <div class="form-control">
+          <label for="text">Text</label>
+          <input type="text" id="text" 
+            data-selector="text"
+          placeholder="Enter text..." />
+        </div>
+        <div class="form-control">
+          <label for="amount"
+            >Amount <br />
+            (negative - expense, positive - income)</label
+          >
+          <input type="number" id="amount" data-selector="amount"
+            placeholder="Enter amount..." />
+        </div>
+        <button class="btn" data-selector="add-transaction" ref="add-transaction" @click="handleClick">Add transaction</button>
+      </form>
+    </div>
+</template>
+
+
+<script lang="ts">
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
+
+export default {
+  name: "App",
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+    }
+  }
+}
 
 type Transaction = {
     readonly id: string,
@@ -139,46 +190,6 @@ const init = async () => {
 init()
 </script>
 
-<template>
-    <div class="container">
-      <h4>Your Balance</h4>
-      <h1 id="balance" data-selector="balance">$0.00</h1>
-
-      <div class="inc-exp-container">
-        <div>
-          <h4>Income</h4>
-          <p id="money-plus" data-selector="income" class="money plus">+$0.00</p>
-        </div>
-        <div>
-          <h4>Expense</h4>
-          <p id="money-minus" data-selector="expense" class="money minus">-$0.00</p>
-        </div>
-      </div>
-
-      <h3>History</h3>
-      <ul id="list" class="list" data-selector="history">
-      </ul>
-
-      <h3>Add new transaction</h3>
-      <form id="form">
-        <div class="form-control">
-          <label for="text">Text</label>
-          <input type="text" id="text" 
-            data-selector="text"
-          placeholder="Enter text..." />
-        </div>
-        <div class="form-control">
-          <label for="amount"
-            >Amount <br />
-            (negative - expense, positive - income)</label
-          >
-          <input type="number" id="amount" data-selector="amount"
-            placeholder="Enter amount..." />
-        </div>
-        <button class="btn" data-selector="add-transaction">Add transaction</button>
-      </form>
-    </div>
-</template>
 
 <style scoped>
 .logo {
